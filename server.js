fd9cae17d31f00f066a64989df8424f59067b011
ConnerKT <connerthompson121@gmail.com>
2023-06-11 22:14:11 -0500

    fixing mongoose error, tbd

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
-require('dotenv').config()
+require('dotenv').config();
+
+const PostData = require('./model/post');
 
 // Requiring express after installing it
-const express = require('express')
+const express = require('express');
 
 // Envoking the function
 const app = express();
@@ -11,13 +13,25 @@ app.use(express.json());
 //Using the next parameter lets us use different middleware,
 //without it our middleware would get stuck at the first one.
 app.use((req, res, next) => {
-    console.log(req.path, req.method)
+    console.log(req.path, req.method);
     next();
-})
+});
 
 //Setting up a basic route
+app.get('/fill' , async(req, res) => {
+    try{
+       PostData();
+       res.send("Data filled, check your database!");
+    }catch(error){
+        res.send('Error, please check your server');
+    }
+});
 
+//Test Route
+app.get('/test', (req, res) => {
+    res.send('This means that it works :)');
+});
 //Listen for request
 app.listen(process.env.PORT, () =>{
-    console.log(`Listening on Port`, process.env.PORT)
+    console.log(` 🔅 Listening on Port`, process.env.PORT);
 });
