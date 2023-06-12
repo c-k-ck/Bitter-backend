cbc07ba97b7d8c01e97327100034e3f700380755
Kelsee Watts <123975076+chillgatez@users.noreply.github.com>
2023-06-12 18:51:54 -0500

    added user profile model

--- model/userprofile.js
+++ model/userprofile.js
@@ -1,10 +1,36 @@
 //Modularize your code by putting your schema and model in its own separate file and requiring the schema into your server.
 const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_DB);
 
 const userSchema = new mongoose.Schema({
-    name: {type: String, require: true},
-    email: {type: String, require: true},
-    hometown: {type: String, require: true},
-    age: {type: Number, require: true},
-    bio: {type: String, require: true},
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    hometown: { type: String },
+    age: { type: Number },
+    bio: { type: String },
 });
+
+const User = new mongoose.model("User", userSchema);
+const testUser = new User({
+    name: 'Ms. Ruby',
+    email: 'ms.ruby@gmail.com',
+    hometown: 'Memphis',
+    age: 22,
+    bio: "Ms. Ruby sings the blues so you don't have to",
+});
+
+mongoose.connection.on('error', (error) => {
+    console.error('Error connecting to MongoDB:', error);
+});
+
+mongoose.connection.once('open', async () => {
+    console.log('Connected to MongoDB');
+
+    try {
+        await testUser.save();
+    } catch (error) {
+        console.error("Error creating user:", error)
+    }
+});
+
+module.exports = User;
