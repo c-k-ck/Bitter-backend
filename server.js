e63d392da0d8c124ba08ea6d2bbcbff908e96a45
ConnerKT <connerthompson121@gmail.com>
2023-06-12 20:52:35 -0500

    like implementation & categories
    Add categories for schema
    Change it to description for the schema instead of body
    Add likes to schema
    Leverage development for user management

--- server.js
+++ server.js
@@ -1,38 +1,25 @@
 require('dotenv').config();
-
 // Requiring express after installing it
 const express = require('express');
-
 // Envoking the function
 const app = express();
-
 //Invoking cors to protect use security on our routes
 const cors = require('cors');
-
-//Setting up mongoose in our application
-const mongoose = require('mongoose');
-
 //Importing the routes we have
 const postRoutes = require('./routes/post');
 const userRoutes = require('./routes/user');
-
-
 //Middleware
 app.use(express.json());
 app.use(cors());
-
 // Setting up the Routes
 app.use('/post', postRoutes)
 app.use('/user', userRoutes)
-
-
 //Using the next parameter lets us use different middleware,
 //without it our middleware would get stuck at the first one.
 app.use((req, res, next) => {
     console.log(req.path, req.method);
     next();
 });
-
 //Test Route
 app.get('/test', (req, res) => {
     res.send('This means that it works :)');
